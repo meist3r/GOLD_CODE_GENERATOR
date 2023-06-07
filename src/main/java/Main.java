ccf633883b85711356bf7509adcaf7b33412301d
Artur Grochal <arturgrochal2805@gmail.com>
2023-06-07 17:25:30 +0200

    added info about seed length

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*int[] data = {-1, -1, 7, 7, -9, 7, -9, -1, -1, -1, 7, 7, -1, 7, -9, -9, -1, 7, -1, -9, -1, -1, -1, -1, -9, -1, -1, 7, -1, 7, 7};
-        LineChart.generateLineChart(data, "Autokorelacja krzyżowa");*/
-        Scanner scanner = new Scanner(System.in);
 
-        int choice = 1;
-        while(true){
+        int choice;
+        while (true) {
             choice = mainMenu();
             int result[][];
-            switch(choice){
+            switch (choice) {
                 case 1:
                     result = definedMenu();
-                    if(result==null){
+                    if (result == null) {
                         continue;
                     }
                     actionMenu(result);
                     break;
                 case 2:
                     result = undefinedMenu();
-                    if(result==null){
+                    if (result == null) {
                         continue;
                     }
                     actionMenu(result);
@@ -35,7 +32,7 @@ public class Main {
         }
     }
 
-    public static int mainMenu(){
+    public static int mainMenu() {
         Scanner scanner = new Scanner(System.in);
         int choice;
         System.out.println("Generator kodow Golda by Artur Grochal & Michal Wysocki");
@@ -47,38 +44,43 @@ public class Main {
         return choice;
     }
 
-    public static void actionMenu(int [][] data){
+    public static void actionMenu(int[][] data) {
         Scanner scanner = new Scanner(System.in);
         int choice;
-        Generator g = new Generator(data[0],data[1],data[2],data[3]);
+        Generator g = new Generator(data[0], data[1], data[2], data[3]);
         Validator validator = new Validator(g);
-        while(true){
-        System.out.println("1. Pokaz wykres korelacji krzyzowej");
-        System.out.println("2. Pokaz wykres autokorelacji");
-        System.out.println("3. Generuj kolejne kody Golda");
-        System.out.print("\n> ");
-        choice = scanner.nextInt();
-        switch(choice){
-            case 1:
-                int[] mSeqCorArr = validator.getMSequencesCorrelation();
-                LineChart.generateLineChart(Utils.intToDouble(mSeqCorArr), "Korelacja krzyżowa");
-                break;
-            case 2:
-                int[] mSeqAutoCorArr = validator.getAutoCorrelation();
-                LineChart.generateLineChart(Utils.intToDouble(mSeqAutoCorArr), "Autokorelacja");
-                break;
-            case 3:
-                int goldCode = g.generate();
-                System.out.println("# " + goldCode);
-                // TODO: JAKIS LOOP
-                break;
-            default:
-                break;
-        }
+        boolean preferred = validator.isPreferredSequences();
+        System.out.println("Twoja para wielomianow jest: " + (preferred ? "OPTYMALNA" : "NIEOPTYMALNA"));
+        while (true) {
+            System.out.println("1. Pokaz wykres korelacji krzyzowej");
+            System.out.println("2. Pokaz wykres autokorelacji");
+            System.out.println("3. Generuj kod Golda");
+            System.out.print("\n> ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    int[] mSeqCorArr = validator.getMSequencesCorrelation();
+                    LineChart.generateLineChart(Utils.intToDouble(mSeqCorArr), "Korelacja krzyżowa");
+                    break;
+                case 2:
+                    int[] mSeqAutoCorArr = validator.getAutoCorrelation();
+                    LineChart.generateLineChart(Utils.intToDouble(mSeqAutoCorArr), "Autokorelacja");
+                    break;
+                case 3:
+                    System.out.print("# ");
+                    for (int i = 0; i < g.getLengthOfOptimalGoldCode(); i++) {
+                        int goldCode = g.generate();
+                        System.out.print(goldCode);
+                    }
+                    System.out.println();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
-    public static int[][] definedMenu(){
+    public static int[][] definedMenu() {
         int[][] result = new int[4][];
         Scanner scanner = new Scanner(System.in);
         int pairNum;
@@ -92,50 +94,45 @@ public class Main {
         System.out.print("\n> ");
         pairNum = scanner.nextInt();
 
-        if(pairNum<7 && pairNum>0) {
+        if (pairNum < 7 && pairNum > 0) {
             int[] pair1 = {};
             int[] pair2 = {};
             switch (pairNum) {
                 case 1:
-                    pair1 = new int[]{2,3,4,5};
-                    pair2 = new int[]{2,5};
+                    pair1 = new int[]{2, 3, 4, 5};
+                    pair2 = new int[]{2, 5};
                     break;
                 case 2:
-                    pair1 = new int[]{1,2,5,6};
-                    pair2 = new int[]{1,6};
+                    pair1 = new int[]{1, 2, 5, 6};
+                    pair2 = new int[]{1, 6};
                     break;
                 case 3:
-                    pair1 = new int[]{1,2,3,7};
-                    pair2 = new int[]{3,7};
+                    pair1 = new int[]{1, 2, 3, 7};
+                    pair2 = new int[]{3, 7};
                     break;
                 case 4:
-                    pair1 = new int[]{1,3,7};
-                    pair2 = new int[]{1,7};
+                    pair1 = new int[]{1, 3, 7};
+                    pair2 = new int[]{1, 7};
                     break;
                 case 5:
-                    pair1 = new int[]{3,4,6,9};
-                    pair2 = new int[]{4,9};
+                    pair1 = new int[]{3, 4, 6, 9};
+                    pair2 = new int[]{4, 9};
                     break;
                 case 6:
-                    pair1 = new int[]{2,5,8,11};
-                    pair2 = new int[]{2,11};
+                    pair1 = new int[]{2, 5, 8, 11};
+                    pair2 = new int[]{2, 11};
                     break;
                 default:
                     break;
             }
             System.out.println();
 
-            int[][]seeds = getSeeds();
-
             int maxMSeq1 = Arrays.stream(pair1).max().getAsInt();
             int maxMSeq2 = Arrays.stream(pair2).max().getAsInt();
 
             int seedLimit = (maxMSeq1 > maxMSeq2) ? maxMSeq1 : maxMSeq2;
 
-            if(seeds[0].length!=seedLimit || seeds[1].length !=seedLimit){
-                System.out.println("Bledna dlugosc ziaren");
-                return null;
-            }
+            int[][] seeds = getSeeds(seedLimit);
 
             result[0] = pair1;
             result[1] = pair2;
@@ -151,7 +148,7 @@ public class Main {
 
     }
 
-    public static int[][] undefinedMenu(){
+    public static int[][] undefinedMenu() {
         int[][] result = new int[4][];
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj ilosc skladnikow pierwszego wielomianu");
@@ -160,7 +157,7 @@ public class Main {
         System.out.println("Podaj pierwszy wielomian generujacy, wypisujac wykladniki w nim wystepujace np. 5 4 3 2:");
         System.out.print("\n> ");
         int[] mSeq1 = new int[mSeqLen1];
-        for(int i=0;i<mSeqLen1;i++){
+        for (int i = 0; i < mSeqLen1; i++) {
             mSeq1[i] = scanner.nextInt();
         }
         System.out.println();
@@ -170,7 +167,7 @@ public class Main {
         System.out.println("Podaj drugi wielomian generujacy w tym samym formacie:");
         System.out.print("\n> ");
         int[] mSeq2 = new int[mSeqLen2];
-        for(int i=0;i<mSeqLen2;i++){
+        for (int i = 0; i < mSeqLen2; i++) {
             mSeq2[i] = scanner.nextInt();
         }
 
@@ -179,17 +176,12 @@ public class Main {
         mSeq1 = Utils.reverseArray(mSeq1);
         mSeq2 = Utils.reverseArray(mSeq2);
 
-        int[][]seeds = getSeeds();
-
         int maxMSeq1 = Arrays.stream(mSeq1).max().getAsInt();
         int maxMSeq2 = Arrays.stream(mSeq2).max().getAsInt();
 
         int seedLimit = (maxMSeq1 > maxMSeq2) ? maxMSeq1 : maxMSeq2;
 
-        if(seeds[0].length!=seedLimit || seeds[1].length !=seedLimit){
-            System.out.println("Bledna dlugosc ziaren");
-            return null;
-        }
+        int[][] seeds = getSeeds(seedLimit);
 
         result[0] = mSeq1;
         result[1] = mSeq2;
@@ -200,28 +192,34 @@ public class Main {
 
     }
 
-    public static int[][] getSeeds(){
+    public static int[][] getSeeds(int seedLimit) {
         Scanner scanner = new Scanner(System.in);
         int[][] result = new int[2][];
         boolean waitForGoodInput = true;
         int[] seed1 = {};
         int[] seed2 = {};
-        while(waitForGoodInput){
-            System.out.println("Podaj pierwsza wartosc ziarna w formacie Big Endian (najstarszy bit po lewej) np. 0001:");
+        while (waitForGoodInput) {
+            System.out.println("Podaj pierwsza wartosc ziarna o dlugosci " + seedLimit + " w formacie Big Endian (najstarszy bit po lewej) np. 0001:");
             System.out.print("\n> ");
             String seedInput = scanner.next();
             seed1 = extractSeed(seedInput);
-            if(seed1==null){
+            if (seed1 == null) {
                 System.out.println("Bledny format rejestru");
+                continue;
+            } else if(seed1.length!=seedLimit){
+                System.out.println("Bledna dlugosc ziarna");
                 continue;
             }
             System.out.println();
-            System.out.println("Podaj druga wartosc ziarna w tym samym formacie:");
+            System.out.println("Podaj druga wartosc ziarna o dlugosci " + seedLimit + " w tym samym formacie:");
             System.out.print("\n> ");
             seedInput = scanner.next();
             seed2 = extractSeed(seedInput);
-            if(seed2==null){
+            if (seed2 == null) {
                 System.out.println("Bledny format rejestru");
+                continue;
+            } else if(seed2.length!=seedLimit){
+                System.out.println("Bledna dlugosc ziarna");
                 continue;
             }
             System.out.println();
@@ -233,13 +231,13 @@ public class Main {
         return result;
     }
 
-    public static int[] extractSeed(String seed){
+    public static int[] extractSeed(String seed) {
         int[] result = new int[seed.length()];
-        for (int i=0;i<seed.length();i++) {
+        for (int i = 0; i < seed.length(); i++) {
             char c = seed.charAt(i);
-            if(c=='0'){
+            if (c == '0') {
                 result[i] = 0;
-            } else if(c=='1'){
+            } else if (c == '1') {
                 result[i] = 1;
             } else {
                 return null;
