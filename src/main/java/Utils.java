d3905ebb256fca62fe363a436b7ad94c4acf535a
Artur Grochal <arturgrochal2805@gmail.com>
2023-06-10 13:16:36 +0200

    maxIntArr

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -79,6 +79,18 @@ public class Utils {
         return array1;
     }
 
+    public static int maxIntArr(int [] arr){
+        if(arr.length==0) return 0;
+        int res = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>res){
+                res = arr[i];
+            }
+        }
+        return res;
+
+    }
+
 
 
 }
